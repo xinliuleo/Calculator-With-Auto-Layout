d5ae89dfc7b06ee0583ed23859e6cc47b41f79b2
Leo.Liu <xinliu.leo@gmail.com>
2019-06-03 09:49:15 +1000

    trunk

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Leo Liu on 31/5/19.
+//  Copyright © 2019 Leo Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
